6b8f20ae968cbc8c529ba65eabb942e7b596e955
hainiu <1637980653@qq.com>
2022-05-11 17:17:28 +0800

    two commit

--- src/main/java/at/hainiu/Tt01git.java
+++ src/main/java/at/hainiu/Tt01git.java
@@ -3,5 +3,7 @@ package at.hainiu;
 public class Tt01git {
     public static void main(String[] args) {
         System.out.println("你好啊!!!!!小git");
+        System.out.println("你好啊!!!!!小git第二版");
+
     }
 }
